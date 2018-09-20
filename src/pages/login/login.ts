edb6cb1b666bb686fc08eaa1ce4ae9eeb4245613
DoctorRu <vjblan@gmail.com>
2018-09-20 19:07:57 -0300

    sign up and login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import {NavController, ToastController} from 'ionic-angular';
 import {SignupPage} from "../signup/signup";
+import firebase from 'firebase';
+
 
 @Component({
     selector: 'page-login',
@@ -8,11 +10,47 @@ import {SignupPage} from "../signup/signup";
 })
 export class LoginPage {
     
-    constructor(public navCtrl: NavController) {
+    email: string = '';
+    password: string = '';
     
+    constructor(public navCtrl: NavController,
+                public toastCtrl: ToastController) {
+        
     }
     
     gotoSignUp() {
         this.navCtrl.push(SignupPage);
     }
+    
+    
+    login() {
+        firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+            .then(res => {
+                this.toastCtrl.create({
+                        message: 'Welcome ' + res.user.displayName,
+                        duration: 3000
+                    })
+                    .present();
+            })
+            .catch(err => {
+                this.toastCtrl.create({
+                        message: err.message,
+                        duration: 3000
+                    })
+                    .present();
+            });
+    }
 }
+    
+
+
+
+
+
+
+
+
+
+
+
+
--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
-import {NavController, NavParams} from 'ionic-angular';
+import {AlertController, NavController, NavParams, ToastController} from 'ionic-angular';
+import firebase from 'firebase';
 
 @Component({
     selector: 'page-signup',
@@ -7,11 +8,58 @@ import {NavController, NavParams} from 'ionic-angular';
 })
 export class SignupPage {
     
-    constructor(public navCtrl: NavController, public navParams: NavParams) {
+    name: string = '';
+    email: string = '';
+    password: string = '';
+    
+    
+    constructor(public navCtrl: NavController,
+                public navParams: NavParams,
+                public toastCtrl: ToastController,
+                public alertCtrl: AlertController) {
     }
     
-    ionViewDidLoad() {
-        console.log('ionViewDidLoad SignupPage');
+    signup() {
+        firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+            .then(data => {
+                console.log(data);
+                
+                let newUser: firebase.User = data.user;
+                
+                newUser.updateProfile({
+                        displayName: this.name,
+                        photoURL: ""
+                    })
+                    .then(res => {
+                        this.alertCtrl.create({
+                                title: 'Account Created',
+                                message: 'Your account has been created successfully.',
+                                buttons: [
+                                    {
+                                        text: 'OK',
+                                        handler: () => {
+                                        }
+                                    }
+                                ]
+                            })
+                            .present();
+                    })
+                    .catch(err => {
+                        this.toastCtrl.create({
+                                message: err.message,
+                                duration: 3000
+                            })
+                            .present();
+                    })
+                
+            })
+            .catch(err => {
+                this.toastCtrl.create({
+                        message: err.message,
+                        duration: 3000
+                    })
+                    .present();
+            })
     }
     
     
