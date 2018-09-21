b5840a83c5ee0ed19be29dc35234bf22878592cb
DoctorRu <vjblan@gmail.com>
2018-09-21 00:44:42 -0300

    Loading feed with infinite scroll

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
 import firebase from "firebase";
 import moment from "moment";
+import * as _ from 'lodash';
 
 @Component({
     selector: 'page-feed',
@@ -11,6 +12,8 @@ export class FeedPage {
     
     message: string = '';
     posts: any[] = [];
+    pageSize: number = 3;
+    cursor: any;
     
     constructor(public navCtrl: NavController,
                 public navParams: NavParams) {
@@ -30,27 +33,65 @@ export class FeedPage {
                 
                 this.getPosts();
             })
-            .catch(err => { console.log('err ', err)})
+            .catch(err => {
+                console.log('err ', err)
+            })
     }
     
     getPosts() {
         this.posts = [];
         
-        firebase.firestore().collection("posts").orderBy("created","desc").get()
-            .then( docs => {
+        firebase.firestore().collection("posts")
+            .orderBy("created", "desc")
+            .limit(this.pageSize)
+            .get()
+            .then(docs => {
                 
-                docs.forEach( doc=> {
+                docs.forEach(doc => {
+                    this.posts.push(doc);
+                });
+                
+                this.cursor = _.last(this.posts);
+                console.log('cursor ', this.cursor);
+                
+                // console.log('posts', this.posts);
+                
+            })
+            .catch(err => console.log('err ', err))
+        
+    }
+    
+    
+    loadMorePosts(event) {
+        
+        firebase.firestore().collection("posts")
+            .orderBy("created", "desc")
+            .startAfter(this.cursor)
+            .limit(this.pageSize)
+            .get()
+            .then(docs => {
+                
+                docs.forEach(doc => {
                     this.posts.push(doc);
                 });
                 
                 console.log('posts', this.posts);
-            
+                
+                if (docs.size < this.pageSize) {
+                    event.enable(false);
+                } else {
+                    event.complete();
+                    this.cursor = _.last(this.posts);
+    
+                }
+                
             })
-            .catch( err => console.log('err ', err ))
+            .catch(err => console.log('err ', err))
     }
     
     ago(time) {
         let difference = moment(time).diff(moment());
         return moment.duration(difference).humanize();
     }
+    
 }
