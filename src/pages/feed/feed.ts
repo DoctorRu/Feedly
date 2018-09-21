ff0d4f7a9a9cc7e15271b7a4b8d15d0e630151cb
DoctorRu <vjblan@gmail.com>
2018-09-21 00:19:38 -0300

    Retrieve posts / calculating time

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
 import firebase from "firebase";
+import moment from "moment";
 
 @Component({
     selector: 'page-feed',
@@ -9,21 +10,47 @@ import firebase from "firebase";
 export class FeedPage {
     
     message: string = '';
+    posts: any[] = [];
     
     constructor(public navCtrl: NavController,
                 public navParams: NavParams) {
+        
+        this.getPosts();
     }
     
     post() {
         firebase.firestore().collection("posts").add({
-                text: this.message,
+                message: this.message,
                 created: firebase.firestore.FieldValue.serverTimestamp(),
                 owner: firebase.auth().currentUser.uid,
                 owner_name: firebase.auth().currentUser.displayName
             })
             .then(doc => {
                 console.log('created ', doc);
+                
+                this.getPosts();
             })
             .catch(err => { console.log('err ', err)})
+    }
+    
+    getPosts() {
+        this.posts = [];
+        
+        firebase.firestore().collection("posts").orderBy("created","desc").get()
+            .then( docs => {
+                
+                docs.forEach( doc=> {
+                    this.posts.push(doc);
+                });
+                
+                console.log('posts', this.posts);
+            
+            })
+            .catch( err => console.log('err ', err ))
+    }
+    
+    ago(time) {
+        let difference = moment(time).diff(moment());
+        return moment.duration(difference).humanize();
     }
 }
