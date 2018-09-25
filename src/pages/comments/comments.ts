8ae39bb68bba3d82978981f2e7625b98f209e89e
DoctorRu <vjblan@gmail.com>
2018-09-24 23:23:09 -0300

    displaying comments ordered by data

--- src/pages/comments/comments.ts
+++ src/pages/comments/comments.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
-import {NavController, NavParams} from 'ionic-angular';
+import {NavController, NavParams, ViewController} from 'ionic-angular';
+import * as firebase from "firebase";
+import moment from 'moment';
 
 @Component({
     selector: 'page-comments',
@@ -9,13 +11,35 @@ import {NavController, NavParams} from 'ionic-angular';
 export class CommentsPage {
     
     post: any = [];
+    comments: any = [];
     
     constructor(public navCtrl: NavController,
-                public navParams: NavParams
+                public navParams: NavParams,
+                private viewCtrl: ViewController
     ) {
-        
         this.post = this.navParams.get("post");
         console.log('post> ', this.post);
+        
+        firebase.firestore().collection("comments")
+            .where("post", "==", this.post.id)
+            .orderBy("created","desc")
+            .get()
+            .then(data => {
+                this.comments = data.docs;
+            })
+            .catch(err => {
+                console.log('get comments err>', err)
+            })
     }
     
+    
+    ago(time) {
+        let difference = moment(time).diff(moment());
+        return moment.duration(difference).humanize();
+    }
+    
+    
+    close(){
+        this.viewCtrl.dismiss();
+    }
 }
