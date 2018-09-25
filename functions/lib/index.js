d414a51fa3815bae6be4642855eb234faef3c945
DoctorRu <vjblan@gmail.com>
2018-09-24 22:28:20 -0300

    Comments count

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,4 +1,12 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
@@ -31,4 +39,21 @@ exports.updateLikesCount = functions.https.onRequest((request, response) => {
     })
         .catch(err => console.log('trigger get error: ', err));
 });
+exports.updateCommentsCount = functions.firestore.document('comments/{commentId}')
+    .onCreate((event) => __awaiter(this, void 0, void 0, function* () {
+    let data = event.data();
+    let postId = data.post;
+    let doc = yield admin.firestore().collection("posts").doc(postId).get();
+    if (doc.exists) {
+        let commentsCount = doc.data().commentsCount || 0;
+        commentsCount++;
+        yield admin.firestore().collection("posts").doc(postId).update({
+            "commentsCount": commentsCount
+        });
+        return true;
+    }
+    else {
+        return false;
+    }
+}));
 //# sourceMappingURL=index.js.map
